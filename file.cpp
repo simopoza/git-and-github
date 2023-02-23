d2eb20056ed3af227a3bdc00eda5b882b5c147cb
simopoza <mannahri@student.1337.ma>
2023-02-23 14:19:18 +0100

    added todolist

--- file.cpp
+++ file.cpp
@@ -36,3 +36,13 @@
 //     f1();
 //     system("leaks a.out");
 // }
+
+MY TO-DO LIST
+
+1. Write an article.
+2. Code.
+3. Study books.
+4. Attend classes on time.
+5. Visit aunt.
+6. Apply for remote jobs. 
+7. Practice code
